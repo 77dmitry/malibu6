edd4c2d9c393de6afeea82e95a7ff7f57ab04144
a18805183 <dmitryishmuratov@gmail.com>
2020-12-01 19:38:20 +0300

    Initial commit

---
+++ src/main/java/com/burst/malibu69/Malibu69Application.java
@@ -0,0 +1,13 @@
+package com.burst.malibu69;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Malibu69Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Malibu69Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/burst/malibu69/Malibu69ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burst.malibu69;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Malibu69ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
